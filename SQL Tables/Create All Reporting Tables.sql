52268b47182d79d79f1e1b5986070729fbfa8412
Raleldor <raleldor@gmail.com>
2024-09-27 17:20:18 -0400

    Add approved amount to WF

--- SQL Tables/Create All Reporting Tables.sql
+++ SQL Tables/Create All Reporting Tables.sql
@@ -701,7 +701,8 @@ DECLARE @wf_table TABLE (
 	invoice_total DECIMAL(14,2), invoice_amount_paid DECIMAL(14,2), 
 	invoice_sales_tax DECIMAL(14,2),material_cost DECIMAL(14,2),
 	labor_cost DECIMAL(14,2), equipment_cost DECIMAL(14,2),
-	other_cost DECIMAL(14,2), overhead_cost DECIMAL(14,2)
+	other_cost DECIMAL(14,2), overhead_cost DECIMAL(14,2),
+	approved_amount DECIMAL(14,2)
 );
 
 INSERT INTO @wf_table 
@@ -715,7 +716,8 @@ SELECT
 	jc.labor_cost * -1 as labor_cost,
 	jc.equipment_cost * -1 as equipment_cost,
 	jc.other_cost * -1 as other_cost,
-	jc.overhead_amount * -1 as overhead_cost
+	jc.overhead_amount * -1 as overhead_cost,
+	c.appamt as approved_amount
 FROM ',QUOTENAME(@Client_DB_Name),'.dbo.actrec a
 LEFT JOIN (
 	SELECT
@@ -822,6 +824,12 @@ FROM (
 		job_number,
 		''Overhead Cost'' as waterfall_category,
 		overhead_cost as waterfall_value
+	FROM @wf_table
+	UNION ALL
+	SELECT 
+		job_number,
+		''Approved Amount'' as waterfall_category,
+		approved_amount as waterfall_value
 	FROM @wf_table
 ) wf 
 WHERE waterfall_value < 0 OR waterfall_value > 0
