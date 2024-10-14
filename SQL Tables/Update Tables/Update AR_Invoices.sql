9662b58cb6ceedee9ad5c9245d18488dd59c97ec
Raleldor <raleldor@gmail.com>
2024-10-14 14:50:30 -0400

    Recategorize data, add update query

---
+++ SQL Tables/Update Tables/Update AR_Invoices.sql
@@ -0,0 +1,116 @@
+--Specify Client DB Name
+DECLARE @Client_DB_Name NVARCHAR(50) = 'Nvision';  
+--Specify Reporting DB Name
+DECLARE @Reporting_DB_Name NVARCHAR(50) = QUOTENAME(CONCAT(@Client_DB_Name, ' Reporting'));
+SET @Client_DB_Name = QUOTENAME(@Client_DB_Name);
+--Initial variable declaration
+DECLARE @SqlTempTbl NVARCHAR(MAX);
+DECLARE @SqlInsertQuery NVARCHAR(MAX);
+DROP TABLE IF EXISTS #TempTbl
+
+--Update AR_Invoices Table
+--Step 1. Temp table containing reporting table
+SET @SqlTempTbl = CONCAT(N'
+SELECT * INTO #TempTbl FROM ',@Reporting_DB_Name,N'.dbo.AR_Invoices;
+ALTER TABLE #TempTbl
+DROP COLUMN is_deleted,
+DROP COLUMN deleted_date;
+')
+EXECUTE sp_executesql @SqlTempTbl
+
+--Step 2. delete existing reporting table data and replace with updated values
+SET @SqlInsertQuery = CONCAT(N'
+DELETE FROM ',@Reporting_DB_Name,N'.dbo.AR_Invoices;
+INSERT INTO ',@Reporting_DB_Name,N'.dbo.AR_Invoices
+SELECT 
+	a.recnum as job_number,
+	a.jobnme as job_name,
+	a.phnnum as job_phone_number,
+	a.ntetxt as job_notes,
+	a.addrs1 as job_address1,
+	a.addrs2 as job_address2,
+	a.ctynme as job_city,
+	a.state_ as job_state,
+	a.zipcde as job_zip_code,
+	j_t.dstnme as job_tax_district,
+	jt.typnme as job_type,
+	CASE a.status 
+		WHEN 1 THEN ''Bid''
+		WHEN 2 THEN ''Refused''
+		WHEN 3 THEN ''Contract''
+		WHEN 4 THEN ''Current''
+		WHEN 5 THEN ''Complete''
+		WHEN 6 THEN ''Closed''
+		ELSE ''Other''
+	END as job_status,
+	acrinv.recnum as ar_invoice_id,
+	acrinv.invdte as ar_invoice_date,
+	acrinv.dscrpt as ar_invoice_description,
+	acrinv.invnum as ar_invoice_number,
+	CASE acrinv.status 
+		WHEN 1 THEN ''Open''
+		WHEN 2 THEN ''Review''
+		WHEN 3 THEN ''Dispute''
+		WHEN 4 THEN ''Paid''
+		WHEN 5 THEN ''Void''
+		ELSE ''Other''
+	END as ar_invoice_status,
+	tax.dstnme as ar_invoice_tax_district,
+	te.entnme as tax_entity1,
+	te.taxrt1 as tax_entity1_rate,
+	te2.entnme as tax_entity2,
+	te2.taxrt1 as tax_entity2_rate,
+	acrinv.duedte as ar_invoice_due_date,
+	ISNULL(acrinv.invttl,0) as ar_invoice_total,
+	ISNULL(acrinv.slstax,0) as ar_invoice_sales_tax,
+	ISNULL(acrinv.amtpad,0) as ar_invoice_amount_paid,
+	ISNULL(acrinv.invbal,0) as ar_invoice_balance,
+	ISNULL(acrinv.retain,0) as ar_invoice_retention,
+	CASE acrinv.invtyp 
+		WHEN 1 THEN ''Contract''
+		WHEN 2 THEN ''Memo''
+		ELSE ''Other''
+	END as ar_invoice_type,
+	r.clnnme as client_name,
+	CONCAT(es.fstnme, '' '', es.lstnme) as job_supervisor,
+	CONCAT(e.fstnme, '' '', e.lstnme) as job_salesperson,
+	ISNULL(pmt.amount,0) as ar_invoice_payments_payment_amount,
+	ISNULL(pmt.dsctkn,0) as ar_invoice_payments_discount_taken,
+	ISNULL(pmt.aplcrd,0) as ar_invoice_payments_credit_taken,
+	pmt.chkdte as last_payment_received_date,
+	a.insdte as created_date,
+	0 as is_deleted,
+	null as deleted_date
+FROM ',QUOTENAME(@Client_DB_Name),'.dbo.actrec a
+INNER JOIN ',QUOTENAME(@Client_DB_Name),'.dbo.acrinv acrinv on acrinv.jobnum = a.recnum
+LEFT JOIN (
+	SELECT
+		recnum,
+		sum(amount) as amount,
+		sum(dsctkn) as dsctkn,
+		sum(aplcrd) as aplcrd,
+		max(chkdte) as chkdte
+	FROM ',QUOTENAME(@Client_DB_Name),'.dbo.acrpmt
+	GROUP BY recnum
+) pmt on pmt.recnum = acrinv.recnum
+LEFT JOIN ',QUOTENAME(@Client_DB_Name),'.dbo.taxdst tax on tax.recnum = acrinv.taxdst
+LEFT JOIN ',QUOTENAME(@Client_DB_Name),'.dbo.taxdst j_t on j_t.recnum = a.slstax
+INNER JOIN ',QUOTENAME(@Client_DB_Name),'.dbo.jobtyp jt on jt.recnum = a.jobtyp
+LEFT JOIN ',QUOTENAME(@Client_DB_Name),'.dbo.reccln r on r.recnum = a.clnnum
+LEFT JOIN ',QUOTENAME(@Client_DB_Name),'.dbo.taxent te on te.recnum = tax.entty1
+LEFT JOIN ',QUOTENAME(@Client_DB_Name),'.dbo.taxent te2 on te2.recnum = tax.entty2
+LEFT JOIN ',QUOTENAME(@Client_DB_Name),'.dbo.employ es on es.recnum = a.sprvsr 
+LEFT JOIN ',QUOTENAME(@Client_DB_Name),'.dbo.employ e on e.recnum = a.slsemp
+')
+EXECUTE sp_executesql @SqlInsertQuery
+
+--Step 3. Find any values in Temp Table not in Reporting Table, insert them as records flagged as deleted
+SET @SqlInsertQuery = CONCAT(N'
+INSERT INTO ',@Reporting_DB_Name,N'.dbo.AR_Invoices
+SELECT *, 
+	1 as is_deleted,
+	GETDATE() as deleted_date
+FROM #TempTbl t 
+WHERE t.job_number NOT IN (SELECT job_number FROM ',@Reporting_DB_Name,N'.dbo.AR_Invoice)
+')
+EXECUTE sp_executesql @SqlInsertQuery
