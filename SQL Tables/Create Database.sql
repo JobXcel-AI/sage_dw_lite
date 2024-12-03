7c465c246b1f56a1d0cefa31c81bf782997e29af
mhagberg <mike.hagberg@gmail.com>
2024-12-03 15:18:16 -0700

    Update Create Database.sql
    fixed shrink table

--- SQL Tables/Create Database.sql
+++ SQL Tables/Create Database.sql
@@ -1,25 +1,22 @@
---Specify Client DB Name
-DECLARE @Client_DB_Name NVARCHAR(50) = 'Nvision';  
---Choose Reporting DB Name if desired
+-- Specify Client DB Name
+DECLARE @Client_DB_Name NVARCHAR(50) = 'Vertex Coatings';
+-- Choose Reporting DB Name if desired
 DECLARE @Reporting_DB_Name NVARCHAR(50) = CONCAT(@Client_DB_Name, ' Reporting');
---Define Save Path for Database
+-- Define Save Path for Database
 DECLARE @Reporting_DB_Path NVARCHAR(100) = CONCAT('C:\Sage100Con\Company\', @Client_DB_Name);
 
---Create DB SQL Command
-DECLARE @SqlCommand NVARCHAR(400);
-SET @SqlCommand = CONCAT(N'CREATE DATABASE ', QUOTENAME(@Reporting_DB_Name), ' ON 
-	(NAME = ', QUOTENAME(CONCAT(@Reporting_DB_Name, '_dat')), ', 
-	FILENAME = ''', @Reporting_DB_Path, '\', @Reporting_DB_Name, '_dat.mdf'')
-	LOG ON 
-	(NAME = ', QUOTENAME(CONCAT(@Reporting_DB_Name, '_log')), ', 
-	FILENAME = ''', @Reporting_DB_Path, '\', @Reporting_DB_Name, '_log.ldf'');
-GO
-ALTER DATABASE QUOTENAME(@Reporting_DB_Name) SET AUTO_SHRINK ON
-GO
-')
+-- Create DB SQL Command
+DECLARE @SqlCommand NVARCHAR(MAX);
+SET @SqlCommand = CONCAT(
+  N'CREATE DATABASE ', QUOTENAME(@Reporting_DB_Name), ' ON
+  (NAME = ', QUOTENAME(CONCAT(@Reporting_DB_Name, '_dat')), ',
+  FILENAME = ''', @Reporting_DB_Path, '\', @Reporting_DB_Name, '_dat.mdf'')
+  LOG ON
+  (NAME = ', QUOTENAME(CONCAT(@Reporting_DB_Name, '_log')), ',
+  FILENAME = ''', @Reporting_DB_Path, '\', @Reporting_DB_Name, '_log.ldf'');',
+  'ALTER DATABASE ', QUOTENAME(@Reporting_DB_Name), ' SET AUTO_SHRINK ON;'
+);
 
---Verify SQL Query
---SELECT @SqlCommand  
+-- Execute the SQL Command
 USE master;
-EXECUTE sp_executesql @SqlCommand
-
+EXEC sp_executesql @SqlCommand;
