8f4380d8179b70b5a67641c7779b4a735e7b8ea5
mhagberg <mike.hagberg@gmail.com>
2025-01-02 17:37:39 -0700

    Create nvision_update_script.py
    first commit

---
+++ Nvision/nvision_update_script.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+# Customer-specific variables
+CUSTOMER_NAME = "Nvision"
+CUSTOMER_DB_NAME = "Nvision Reports"
+
+# Path to the centralized script
+base_dir = os.path.dirname(os.path.dirname(__file__))  # Move up to the base directory
+central_script_path = os.path.join(base_dir, "metabase_update_scripts", "update_table_script.py")
+
+# Command to execute the centralized script
+command = [
+    "python3",
+    central_script_path,
+    CUSTOMER_NAME,
+    CUSTOMER_DB_NAME
+]
+
+# Execute the script
+subprocess.run(command)
