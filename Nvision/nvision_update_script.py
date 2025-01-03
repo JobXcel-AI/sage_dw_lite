b173f5d03c2daf9998c2bcdc7d2e9ea96ab8de57
mhagberg <mike.hagberg@gmail.com>
2025-01-03 16:24:34 -0700

    Update nvision_update_script.py

--- Nvision/nvision_update_script.py
+++ Nvision/nvision_update_script.py
@@ -4,17 +4,25 @@ import subprocess
 # Customer-specific variables
 CUSTOMER_NAME = "Nvision"
 CUSTOMER_DB_NAME = "Nvision"
+SQL_SERVER = "nvisionglass.servernova.net"
+SQL_PORT = "50366"
+SQL_USERNAME = "jobxcel"
+SQL_PASSWORD = "bH2RqTYVPgtF4bH9LTD!"
 
 # Path to the centralized script
 base_dir = os.path.dirname(os.path.dirname(__file__))  # Move up to the base directory
 central_script_path = os.path.join(base_dir, "metabase_update_scripts", "update_table_script.py")
 
-# Command to execute the centralized script
+# Command to execute the centralized script with connection details
 command = [
     "python3",
     central_script_path,
     CUSTOMER_NAME,
-    CUSTOMER_DB_NAME
+    CUSTOMER_DB_NAME,
+    SQL_SERVER,
+    SQL_PORT,
+    SQL_USERNAME,
+    SQL_PASSWORD
 ]
 
 # Execute the script
