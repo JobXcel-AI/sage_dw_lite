1c2b85392b939f0abc58105d2405976f717f4665
mike <mike.hagberg@gmail.com>
2025-02-23 20:22:16 -0700

    consolidating when they have more than one DB

--- ASG/asg_update_script.py
+++ ASG/asg_update_script.py
@@ -1,17 +1,19 @@
 import os
 import subprocess
-
 # Customer-specific variables
-CUSTOMER_NAME = "ASG - JS-Ultimate"
-CUSTOMER_DB_NAMES = "JS-Ultimate Demonstration Data"
+CUSTOMER_NAME = "ASG"
+CUSTOMER_DB_NAMES = "Bryan - S100 Ultimate,Andy - S100 Ultimate 1,Craig - S100 Ultimate,Danielle - S100 Ultimate,Catie - S100 Ultimate,Home Builders of FL,Jennifer - S100 Ultimate,JS-Ultimate Demonstration Data,MH-Ultimate Demonstration Data,Nate - S100 Ultimate,Nick - S100 Ultimate,Pat - S100 Ultimate,Sarah - Ultimate,Tanya - S100 Ultimate,Trish - Ultimate,ZB - Ultimate"
 SQL_SERVER = "asgdemo.servernova.net"
+SQL_INSTANCE= "SN-20202"
 SQL_PORT = "65288"
 SQL_USERNAME = "jobxcel"
-SQL_PASSWORD = "*Fr33B1rd77$Y@nk33*"
+SQL_PASSWORD = "jobxcel"
+USE_SSH_TUNNEL = False
+SQL_FILENAME = "Update All Reporting Tables.sql"
 
 # Path to the centralized script
 base_dir = os.path.dirname(os.path.dirname(__file__))  # Move up to the base directory
-central_script_path = os.path.join(base_dir, "metabase_update_scripts", "update_snapshot_monthly.py")
+central_script_path = os.path.join(base_dir, "metabase_update_scripts", "update_sql_script.py")
 
 # Command to execute the centralized script with connection details
 command = [
@@ -20,9 +22,12 @@ command = [
     CUSTOMER_NAME,
     CUSTOMER_DB_NAMES,
     SQL_SERVER,
+    SQL_INSTANCE,
     SQL_PORT,
     SQL_USERNAME,
-    SQL_PASSWORD
+    SQL_PASSWORD,
+    str(USE_SSH_TUNNEL),
+    SQL_FILENAME
 ]
 
 # Execute the script
